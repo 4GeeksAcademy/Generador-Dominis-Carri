011bde3bbc1dc51e798377b2e47936ebc4a6a1cd
José Francisco Carrión Ramos <145589365+JFCR14@users.noreply.github.com>
2025-02-10 04:39:24 +0000

    1.0

--- src/app.js
+++ src/app.js
@@ -1,11 +1,21 @@
-import "bootstrap";
-import "./style.css";
 
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+document.addEventListener("DOMContentLoaded", function () {
+  let pronoun = ['the', 'our'];
+  let adj = ['great', 'big'];
+  let noun = ['jogger', 'racoon'];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+  const listaDominios = document.getElementById("dominios");
+
+  for (let p of pronoun) {
+    for (let a of adj) {
+      for (let n of noun) {
+        let dominio = `${p}${a}${n}.com`;
+        let listItem = document.createElement("li");
+        listItem.textContent = dominio;
+        listItem.classList.add("list-group-item");
+        listaDominios.appendChild(listItem);
+      }
+    }
+  }
+});
